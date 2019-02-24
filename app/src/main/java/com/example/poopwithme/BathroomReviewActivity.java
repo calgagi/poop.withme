02bad80816b0140402c348b699f7ea4a501464af
calgagi <calgagi@gmail.com>
2019-02-23 22:11:39 -0800

    title w google maps implicit intents done

---
+++ app/src/main/java/com/example/poopwithme/BathroomReviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.poopwithme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BathroomReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bathroom_review);
+    }
+}
